801606e54302616587c4899da44b2fde66826c3d
AAAAAAAAAAAARONW <AARONWZJV@OUTLOOK.COM>
2023-10-31 22:57:19 +0000

    Water System is Here!
    In this update, water system is ready, include the real time water shader that enable vivid visual effect of water(reflection etc), and the water interaction will be available and release soon! To use this water system, please drag the water material to the target object and then use tool> create cube map to create a cube map(after import everything). -Aaron

---
+++ Assets/RealTimeWaterSystem/CustomWater.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:323fa2786424aebc86e81b8831fad5289312fa38bc2ec2d05732206b9a54f119
+size 15870
---
+++ Assets/RealTimeWaterSystem/CustomWaterSphere.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5b169c082e35d308520bfcae5c1cda69d566de5745462f03ee44c824e1e6d9e
+size 16354
