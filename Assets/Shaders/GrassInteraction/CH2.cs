72eb5e61072976c724864ce0cec2d3656e033a27
AAAAAAAAAAAARONW <AARONWZJV@OUTLOOK.COM>
2023-11-21 23:05:05 +0000

    Updated Water and Grass Shader

---
+++ Assets/Shaders/GrassInteraction/CH2.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1884bae89ab17ba87b612952260c8eb86ccb2e44a2d06f3fa243b33ee9827a02
+size 810
---
+++ Assets/Shaders/GrassInteraction/GrassInteract.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7bbdd76bd823c0b7511d8388147626fe877a201da57e3b915a15b15bbda5cfe3
+size 298
